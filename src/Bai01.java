00e0f252defb5abfcfca0868e7d89f16883e893b
LeeAnhs06 <anhlx.b24dtcn191@stu.ptit.edu.vn>
2026-02-01 18:19:42 +0700

    Update Bai01.java

--- src/Bai01.java
+++ src/Bai01.java
@@ -1,3 +1,30 @@
-void main() {
-  $END$
+class Student {
+    String maSinhVien;
+    String hoTen;
+    int namSinh;
+    double diemTrungBinh;
+    
+    Student(String maSinhVien, String hoTen, int namSinh, double diemTrungBinh) {
+        this.maSinhVien = maSinhVien;
+        this.hoTen = hoTen;
+        this.namSinh = namSinh;
+        this.diemTrungBinh = diemTrungBinh;
+    }
+    
+    void hienThiThongTin() {
+        System.out.println("Ma sinh vien: " + maSinhVien);
+        System.out.println("Ho ten: " + hoTen);
+        System.out.println("Nam sinh: " + namSinh);
+        System.out.println("Diem trung binh: " + diemTrungBinh);
+    }
+}
+
+public class Bai01 {
+    public static void main(String[] args) {
+        Student sv1 = new Student("SV001", "Nguyen Van A", 2003, 8.5);
+        Student sv2 = new Student("SV002", "Tran Thi B", 2004, 7.8);
+        
+        sv1.hienThiThongTin();
+        sv2.hienThiThongTin();
+    }
 }
